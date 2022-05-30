a4106437038b5a6dafabafa2258d4a1f1375328d
Mason Wagoner <mgwagoner@gmail.com>
2022-05-29 20:14:38 -0400

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// HOMEPAGE
 app.get('/', (req, res) => {
-  res.send('Hello Worl!')
+  res.send('Hello World!')
 })
 
-app.listen(3000)
+// WILDCARD ROUTE
+app.get('*', (req, res) => {
+  res.status(404).send('<h1>404 Page</h1>')
+})
+// wilcard route must be at the bottom of other routes otherwise it will override all other pages
+
+app.listen(process.env.PORT)
+// this pulls the PORT variable from the .env file
