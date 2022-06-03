1991e6e7822488fb563d8e491aba3dabc14836fb
Mason Wagoner <mgwagoner@gmail.com>
2022-06-02 22:52:51 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,25 +1,9 @@
 const router = require('express').Router()
 // the express.Router() function is used to create a new router object in your program to handle requests
+const places = require('../models/places.js')
 
 // GET /places
 router.get('/', (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisines: "Thai, Pan-Asian",
-      pic: "/images/Green-Restaurant-Design.jpg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisines: "Coffee, Bakery",
-      pic: "/images/Tea-House-Inside.jpg",
-      // why do we not have to put public/images... it doesn't render the photo when you write it that way
-    },
-  ]
   res.render('places/index', { places })
 })
 
@@ -30,7 +14,18 @@ router.get('/new', (req, res) => {
 
 router.post('/', (req, res) => {
   console.log(req.body)
-  res.send('POST /places stub')
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = "Fightertown"
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 
 
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: "H-Thai-ML",
+    city: "Seattle",
+    state: "WA",
+    cuisines: "Thai, Pan-Asian",
+    pic: "/images/Green-Restaurant-Design.jpg",
+  }, {
+    name: "Coding Cat Cafe",
+    city: "Phoenix",
+    state: "AZ",
+    cuisines: "Coffee, Bakery",
+    pic: "/images/Tea-House-Inside.jpg",
+  }]
